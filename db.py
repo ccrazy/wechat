3018d548c2a5ddd72eee92cf482fe82491600920
ccrazy <liudeyin001@126.com>
2017-10-31 14:53:21 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import pymysql
 
-database=pymysql.connect(host='ccnuldy322.gotoftp2.com', port=3306, user='ccnuldy322', passwd='ccnuildy',db='ccnuldy322', charset='utf8',
+database=pymysql.connect(host='sever', port=3306, user='username', passwd='password',db='dbname', charset='utf8',
  cursorclass=pymysql.cursors.DictCursor)
  
